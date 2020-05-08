dd4d5e47a42c274de8a00c12440836d30cd4a318
carstumps <carstumps>
2020-05-08 05:08:47 -0400

    Updated

--- app/routes.py
+++ app/routes.py
@@ -137,7 +137,7 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
+        #login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
